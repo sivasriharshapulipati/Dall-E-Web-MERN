cdf5a16172b60cd16adac7964c4255431207efae
Siva Sri Harsha Pulipati <117675778+sivasriharshapulipati@users.noreply.github.com>
2023-07-05 23:04:33 +0530

    ec cors

--- client/src/components/card.jsx
+++ client/src/components/card.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
 
-const card = () => {
-  return (
-    <div>card</div>
-  )
-}
+import { download } from '../assets';
+import { downloadImage } from '../utils';
 
-export default card
+const Card = ({ _id, name, prompt, photo }) => (
+  <div className="relative rounded-xl group shadow-card hover:shadow-cardhover card">
+    <img
+      className="object-cover w-full h-auto rounded-xl"
+      src={photo}
+      alt={prompt}
+    />
+    <div className="group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-[#10131f] m-2 p-4 rounded-md">
+      <p className="overflow-y-auto text-sm text-white prompt">{prompt}</p>
+
+      <div className="flex items-center justify-between gap-2 mt-5">
+        <div className="flex items-center gap-2">
+          <div className="flex items-center justify-center object-cover text-xs font-bold text-white bg-green-700 rounded-full w-7 h-7">{name[0]}</div>
+          <p className="text-sm text-white">{name}</p>
+        </div>
+        <button type="button" onClick={() => downloadImage(_id, photo)} className="bg-transparent border-none outline-none">
+          <img src={download} alt="download" className="object-contain w-6 h-6 invert" />
+        </button>
+      </div>
+    </div>
+  </div>
+);
+
+export default Card;
--- server/index.js
+++ server/index.js
@@ -10,23 +10,24 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({limit:'50mb'}));
+app.use(express.json({ limit: '50mb' }));
 
-app.use('/api/v1/post',postRoutes);
-app.use('api/v1/dalle',dalle);
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
 
-app.get('/' ,async (req,res)=>{
-    res.send('Hello from Dall-E');
-})
+app.get('/', async (req, res) => {
+  res.status(200).json({
+    message: 'Hello from DALL.E!',
+  });
+});
 
-const startServer = async()=>{
-
-    try {
-        connectDB(process.env.MONGODB_URL);
-        app.listen(8080 , () => console.log('server has started on port https://loacalhost:8080'))
-        }catch(error){
-            console.log(error);
-        }
-}
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () => console.log('Server started on port 8080'));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 startServer();
