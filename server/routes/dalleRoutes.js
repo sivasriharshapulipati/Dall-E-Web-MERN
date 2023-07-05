00cc3ddbe3c79c993531549b829a95aebf999447
Siva Sri Harsha Pulipati <117675778+sivasriharshapulipati@users.noreply.github.com>
2023-07-05 21:28:06 +0530

    back f

---
+++ server/routes/dalleRoutes.js
@@ -0,0 +1,9 @@
+import { Express } from "express";
+import * as dotenv from 'dotenv';
+import { Configuration,OpenAIApi } from "openai";
+
+
+
+dotenv.config();
+
+const router = express.Router()
---
+++ server/routes/postRoutes.js
@@ -0,0 +1,9 @@
+import { Express } from "express";
+import * as dotenv from 'dotenv';
+import {v2 as cloudinary} from 'cloudinary';
+
+import Post from '../mongodb/models/post';
+
+dotenv.config();
+
+const router = express.Router()
