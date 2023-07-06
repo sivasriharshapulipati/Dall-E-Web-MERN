30996605042441a4bae60d16d08ac970cc8b17ce
Siva Sri Harsha Pulipati <117675778+sivasriharshapulipati@users.noreply.github.com>
2023-07-06 20:13:54 +0530

    h

--- server/index.js
+++ server/index.js
@@ -9,7 +9,12 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin:"http://localhost:5173",
+  method:["GET","POST"]
+
+}));
+
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes);
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import { v2 as cloudinary } from 'cloudinary';
-
 import Post from '../mongodb/models/post.js';
 
 dotenv.config();
@@ -34,7 +33,7 @@ router.route('/').post(async (req, res) => {
       photo: photoUrl.url,
     });
 
-    res.status(200).json({ success: true, data: newPost });
+    res.status(201).json({ success: true, data: newPost });
   } catch (err) {
     res.status(500).json({ success: false, message: 'Unable to create a post, please try again' });
   }
